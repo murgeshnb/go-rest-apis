d99a81711ee27320dd14607ad9a61727fffc2fb7
murgeshnb <mnbechetti@gmail.com>
2022-07-07 13:21:10 +0530

    initial commit for sample go-rest-api's

---
+++ src/test/java/ExecuteSomething.java
@@ -0,0 +1,9 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ExecuteSomething {
+    @Test
+    public void executeTest(){
+        Assert.assertEquals(1,1);
+    }
+}
