ad07f74eb6e542bb9a5554f5dfad60b6f0fdd576
murgeshnb <mnbechetti@gmail.com>
2022-07-11 16:39:26 +0530

    Added one Test for Creating a new USER

--- src/test/java/ExecuteSomething.java
+++ src/test/java/ExecuteSomething.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -16,5 +17,24 @@ public class ExecuteSomething {
                 .then()
                     .statusCode(200)
                     .log().body();
+    }
+
+    @Test
+    public void createANewUSer(){
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization","Bearer b2ec65fbe6a01c4fdd06094347e0fc47815f17e078af6459aea9cc9e0c511d1b")
+                .body("{\n" +
+                        "  \"name\": \"Tenali Ramakrishna\",\n" +
+                        "  \"gender\": \"male\",\n" +
+                        "  \"email\": \"tenali.ramakrishna3@gmail.com\",\n" +
+                        "  \"status\": \"active\"\n" +
+                        "}")
+                .when()
+                    .post("https://gorest.co.in/public/v2/users")
+                .then()
+                .log().body()
+                .statusCode(201);
     }
 }
