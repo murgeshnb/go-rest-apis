bb0f362e6e84a1b8eb32081793da43aed1659805
murgeshnb <mnbechetti@gmail.com>
2022-07-11 16:00:26 +0530

    First RestAsuured call to get aLL users

--- src/test/java/ExecuteSomething.java
+++ src/test/java/ExecuteSomething.java
@@ -1,9 +1,20 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.*;
+
 public class ExecuteSomething {
     @Test
-    public void executeTest(){
-        Assert.assertEquals(1,1);
+    public void getAllUSers(){
+        //Arrange
+        //Act
+        //Assert
+
+        given()
+                .when()
+                    .get("https://gorest.co.in/public/v2/users")
+                .then()
+                    .statusCode(200)
+                    .log().body();
     }
 }
