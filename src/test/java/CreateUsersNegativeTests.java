4ce18a53a923dfc91c5a838744d5948271717fdd
murgeshnb <mnbechetti@gmail.com>
2022-07-13 16:02:04 +0530

    extracted getAllUsers method in userclient class started using BeforeClass TestNG annotation used UUID to generate unique emailID's

--- src/test/java/CreateUsersNegativeTests.java
+++ src/test/java/CreateUsersNegativeTests.java
@@ -1,22 +1,31 @@
+import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import users.UsersClient;
 
 public class CreateUsersNegativeTests {
+
+    private UsersClient usersClient;
+
+    @BeforeClass
+    public void beforeClass(){
+        usersClient=new UsersClient();
+    }
 
     @Test
     public void shouldNotCreateUserWithInvalidEmailID(){
         //Arrange
         String body=" \"name\": \"Tenali Ramakrishna\",\n"+" \"gender\": \"male\",\n"+" \"email\": \"tenali.ramakrishna8gmail.com\",\n";
         //Act
-        new UsersClient().createUser(body)
+        usersClient.createUser(body)
                 .then()
                 .log().body()
 
                 //Aseert
-                .statusCode(422);
-
-
-
-
+                .statusCode(422)
+        ;
+          //      .body(".field", Matchers.equalTo("email"))
+           //     .body(".message", Matchers.equalTo("is invalid"));
+        ;
     }
 }
--- src/test/java/GetAllUsers.java
+++ src/test/java/GetAllUsers.java
@@ -1,21 +1,27 @@
-import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import users.UsersClient;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.is;
 
 public class GetAllUsers {
+
+    //Arrange
+    private UsersClient usersClient;
+
+    @BeforeClass
+    public void beforeClass(){
+        usersClient=new UsersClient();
+    }
     @Test
     public void getAllUSers(){
-        //Arrange
         //Act
-        //Assert
-
-        given()
-                .when()
-                    .get("https://gorest.co.in/public/v2/users")
+        usersClient.getAllUsers()
                 .then()
+        //Assert
                     .statusCode(200)
                     .log().body()
                     .body("[0].gender",Matchers.equalTo("female"))
@@ -23,6 +29,4 @@ public class GetAllUsers {
 //                    .body(is(10))
 //                    .assertThat().body(Matchers.hasSize(10));
     }
-
-
 }
