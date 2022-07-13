a0ed04e078b4d2a848940214ee02c4d23e590575
murgeshnb <mnbechetti@gmail.com>
2022-07-13 16:02:04 +0530

    added an Dynamic email ID.

--- src/test/java/CreateUSers.java
+++ src/test/java/CreateUSers.java
@@ -1,26 +1,40 @@
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import users.UsersClient;
+
+import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 
 public class CreateUSers {
 
+    private UsersClient usersClient;
+
+    @BeforeClass
+    public void beforeClass(){
+        usersClient=new UsersClient();
+    }
+
     @Test
     public void createNewMaleUSer(){
         //Arrange
-        String body=" \"name\": \"Tenali Ramakrishna\",\n"+" \"gender\": \"male\",\n"+" \"email\": \"tenali.ramakrishna8@gmail.com\",\n";
+        String email= String.format("%s@gmail.com", UUID.randomUUID());
+        
+        String body=String.format(" \"name\": \"Tenali Ramakrishna\",\n"
+                +" \"gender\": \"male\",\n"
+                +" \"email\": \"%s\",\n",email);
 
         //Act
-        new UsersClient().createUser(body)
+        usersClient.createUser(body)
                 .then()
                 .log().body()
         //Assert
                 .statusCode(201)
                 .body("id", Matchers.notNullValue())
-                .body("email",Matchers.equalTo("tenali.ramakrishna8@gmail.com"))
+                .body("email",Matchers.equalTo(String.format("%s",email)))
                 .body("name",Matchers.equalTo("Tenali Ramakrishna"));
 
     }
@@ -28,19 +42,21 @@ public class CreateUSers {
     @Test
     public void createNewFemaleUSer(){
         //Arrange
-        String body=" \"name\": \"Raziya\",\n"+" \"gender\": \"female\",\n"+ " \"email\": \"Raziya11@gmail.com\",\n";
+        String email = String.format("%s@gmail.com", UUID.randomUUID());
+        String body=String.format(" \"name\": \"Raziya\",\n"+
+                " \"gender\": \"female\",\n"+
+                " \"email\": \"%s\",\n",email);
 
         //Act
-        new UsersClient().createUser(body)
+        usersClient.createUser(body)
                 .then()
                 .log().body()
 
         //Assert
                 .statusCode(201)
                 .body("id", Matchers.notNullValue())
-                .body("email",Matchers.equalTo("Raziya11@gmail.com"))
+                .body("email",Matchers.equalTo(email))
                 .body("name",Matchers.equalTo("Raziya"));
-
     }
 
 
--- src/test/java/users/UsersClient.java
+++ src/test/java/users/UsersClient.java
@@ -9,14 +9,20 @@ public class UsersClient {
 
     public Response createUser(String body) {
         return given()
-                .accept(ContentType.JSON)
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer b2ec65fbe6a01c4fdd06094347e0fc47815f17e078af6459aea9cc9e0c511d1b")
-                .body("{\n" +
-                        body+
-                        "  \"status\": \"active\"\n" +
-                        "}")
+                    .accept(ContentType.JSON)
+                    .contentType(ContentType.JSON)
+                    .header("Authorization", "Bearer b2ec65fbe6a01c4fdd06094347e0fc47815f17e078af6459aea9cc9e0c511d1b")
+                    .body("{\n" +
+                            body+
+                            "  \"status\": \"active\"\n" +
+                            "}")
                 .when()
-                .post("https://gorest.co.in/public/v2/users");
+                    .post("https://gorest.co.in/public/v2/users");
+    }
+
+    public Response getAllUsers() {
+        return given()
+                .when()
+                    .get("https://gorest.co.in/public/v2/users");
     }
 }
